11cc59355770405aff8c533a1adf493ce0038a6d
Mohamed Akram <7mohamedakram@gmail.com>
2023-10-05 17:45:08 -0700

    add task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * print a sentence
+ * Return: 0 if exited successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
