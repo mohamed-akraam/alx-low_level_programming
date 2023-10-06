bd047fb3fe3ccd819f24f2e0851e2f7cfbc696ad
Mohamed Akram <7mohamedakram@gmail.com>
2023-10-05 17:41:18 -0700

    add task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 /**
  * main - prints a sentence
- * Return 0 if exited properly
-*/
+ * Return: 0 if exited properly
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
