06794e3be2170e3d717bb3e48360cafd678777d1
Mohamed Akram <7mohamedakram@gmail.com>
2023-10-11 19:28:03 -0700

    add task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers from 0 to 10.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
